dfc9339098f3fcf3aa428524b45733e9a728e4cb
Baezor <64096464+Baezor1@users.noreply.github.com>
2024-02-04 19:43:00 -0600

    ✨ Video Looping

--- app/src/routes/capture/[slug]/+page.svelte
+++ app/src/routes/capture/[slug]/+page.svelte
@@ -1,6 +1,20 @@
 <script>
     import { onMount } from 'svelte';
-  
+
+	export let data;
+    var title = "undefined"
+
+    var videoSource = "opening.mov"
+
+    if (data.slug == "youtube") {
+        title = "Youtube"
+    } else if (data.slug == "tiktok") {
+        title = "TikTok"
+    }
+
+    $: videoPlayer = document.getElementById("player");
+
+
     let streamState = localStorage.getItem('stream-state');
   
     onMount(() => {
@@ -18,14 +32,35 @@
             });
         };
     });
+
+    function changeSource() {
+        if (videoSource == "door_one.mov") {
+            videoSource = "opening.mov"
+        } else {
+            videoSource = "door_one.mov"
+        }
+    }
+
+
 </script>
 
+{#if streamState == "false"}
+
 <div class="container">
-    <h1>Youtube Capture</h1>
+    <h1>{title} Capture</h1>
     <p>Awaiting start command... {streamState}</p>
     <div aria-busy="true"></div>
 </div>
 
+{:else}
+
+<div class="video-container">
+
+    <video autoplay src="/{videoSource}" on:ended={changeSource}></video>
+
+</div>
+
+{/if}
 <style>
     .container {
         text-align: center;
@@ -39,4 +74,11 @@
     .container h1 {
         margin: 0;
     }
+
+    .video-container {
+        width: 100vw;
+        height: 100vh;
+        /* background-color: black; */
+    }
+
 </style>
