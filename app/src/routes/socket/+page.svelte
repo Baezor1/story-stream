a698ede18ad6f8094a0efb6d535fbbcb4ae7ad6d
Baezor <64096464+Baezor1@users.noreply.github.com>
2024-02-11 15:22:58 -0600

    Socket Client

---
+++ app/src/routes/socket/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    const socket = new WebSocket("ws://localhost:7500");
+
+    var messages = ["Awaiting Connection..."]
+
+    socket.addEventListener("open", () => {
+        messages.push("Connected to Server!")
+        messages = messages
+        console.log("Connected!")
+    })
+
+</script>
+
+<h1>Logs</h1>
+{#each messages as log}
+    <p>{log}</p>
+{/each}
